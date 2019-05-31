93651031fd3aaab1c4eee3fc6fd31a6e79d857c5
Павел Артеменко <partyomenko@gmail.com>
2019-05-31 09:38:28 +0300

    Create PngPackCommand.java

---
+++ src/main/java/pngstore/commands/PngPackCommand.java
@@ -0,0 +1,24 @@
+package pngstore.commands;
+
+import pngstore.FileUtil;
+import pngstore.PngStoreManager;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PngPackCommand extends PngCommand {
+	private static final Logger LOGGER = Logger.getLogger(PngPackCommand.class.getName());
+
+	@Override
+	public void execute(Path sourceFile, Path destinationFile) {
+		FileUtil.checkFileExist(sourceFile);
+		PngStoreManager storeManager = getPngStoreManager(sourceFile, destinationFile);
+		try {
+			storeManager.packFileToPng();
+		} catch (IOException ioe) {
+			LOGGER.log(Level.SEVERE, ioe.getMessage(), ioe);
+		}
+	}
+}
