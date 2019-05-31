f872e17ace92cbfa66a95137093db3b364d045f2
Павел Артеменко <partyomenko@gmail.com>
2019-05-31 09:38:06 +0300

    Create PngHelpCommand.java

---
+++ src/main/java/pngstore/commands/PngHelpCommand.java
@@ -0,0 +1,13 @@
+package pngstore.commands;
+
+import java.nio.file.Path;
+
+public class PngHelpCommand extends PngCommand {
+
+	@Override
+	public void execute(Path sourceFile, Path destinationFile) {
+		System.out.println("Usage:\n" +
+						"\tpngstore -p <source file> <destination png file>\n" +
+						"\tpngstore -u <source png file> <destination file>");
+	}
+}
