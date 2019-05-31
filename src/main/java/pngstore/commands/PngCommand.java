c85f0927d46e33c3953c7c4995431e5113c292f5
Павел Артеменко <partyomenko@gmail.com>
2019-05-31 09:37:46 +0300

    Create PngCommand.java

---
+++ src/main/java/pngstore/commands/PngCommand.java
@@ -0,0 +1,12 @@
+package pngstore.commands;
+
+import pngstore.PngStoreManager;
+
+import java.nio.file.Path;
+
+public abstract class PngCommand implements Command {
+	protected PngStoreManager getPngStoreManager(Path sourceFile, Path destinationFile) {
+		return new PngStoreManager(sourceFile, destinationFile);
+	}
+
+}
